57aaebeaaaac41136733d22d6fb894e235ff3a6c
Sophie Li <sophie.qx.li@gmail.com>
2025-04-19 13:24:15 -0700

    Add catparam.py only

---
+++ catparam.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LearnableJointCategorical(nn.Module):
+    def __init__(self, num_classes): 
+        super().__init__()  
+        self.l = num_classes
+        self.lambdas = nn.Parameter(torch.randn(self.l - 1)) #from 2 to self.l inclusive 
+
+    def getjoints(self, p_u, p_v, method = "midpoint"):
+        assert p_u.shape == p_v.shape
+        assert p_u.shape[0] == self.l
+
+        Pl_prev = None
+        for level in range(2, self.l + 1):
+            
+            pu_sum_prev = p_u[:level-1].sum()
+            pv_sum_prev = p_v[:level-1].sum()
+
+            pu_sum = pu_sum_prev + p_u[level-1]
+            pv_sum = pv_sum_prev + p_v[level-1]
+
+            #acceptable range for lambda
+            lower = max(0, pu_sum_prev / pu_sum + pv_sum_prev / pv_sum - 1)
+            upper = min(pu_sum_prev / pu_sum, pv_sum_prev / pv_sum)
+
+            #Choose fixed lambda in range (later, make it trainable)
+            if method == "midpoint":
+                lambda_scaled = (lower + upper) / 2
+            elif method == "random":
+                lambda_scaled = lower + (upper - lower) * torch.rand(())
+            else:
+                raise ValueError("Method must be 'midpoint' or 'random'")
+
+            print("choosing lambda",level," as:", lambda_scaled)
+            #create the new distribution
+            Pl = torch.zeros((level, level)) 
+
+            if level == 2: 
+                Pl[0,0] = lambda_scaled
+                Pl[0,1] = p_u[0] / (p_u[0] + p_u[1]) - lambda_scaled
+                Pl[1,0] = p_v[0] / (p_v[0] + p_v[1]) - lambda_scaled
+                Pl[1,1] = 1 - Pl[0,0] - Pl[0,1] - Pl[1,0]
+            else: 
+                Pl[:level-1, :level-1] = Pl_prev * lambda_scaled 
+
+                for i in range(level-1): 
+                    Pl[i, -1] = p_u[i]/pu_sum - lambda_scaled * (p_u[i] / pu_sum_prev)
+                for j in range(level-1): 
+                    Pl[-1, j] = p_v[j]/pv_sum - lambda_scaled * (p_v[j] / pv_sum_prev)
+                Pl[-1, -1] = 1 - pu_sum_prev/pu_sum -  pv_sum_prev/pv_sum + lambda_scaled
+            
+            Pl_prev = Pl
+        return Pl_prev
+
+
+
+#SET WHATEVER MARGINALS U WANT HERE
+model = LearnableJointCategorical(num_classes=4)
+p_u = torch.tensor([0.3, 0.2, 0.1, 0.4])
+p_v = torch.tensor([0.4, 0.3, 0.25, 0.05])
+print("u(row)-marginals: ", p_u)
+print("v(col)-marginals: ", p_v)
+joint = model.getjoints(p_u, p_v, method="random")
+
+print(joint)
